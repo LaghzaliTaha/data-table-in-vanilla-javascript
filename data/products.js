2ff5df2a39092da6193fab066edc9c6e16b7c018
tahalaghzali <laghzalitaha0@gmail.com>
2017-05-12 17:03:41 +0100

    first commit

---
+++ data/products.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var products = {
+    list: [
+        {
+            "title": "Savon",
+            "price": 100
+        },
+        {
+            "title": "Parfums",
+            "price": 50
+        },
+        {
+            "title": "Réfrigérateur",
+            "price": 2000
+        },
+        {
+            "title": "Télévision",
+            "price": 3000
+        },
+        {
+            "title": "Ordinateur",
+            "price": 1500
+        },
+        {
+            "title": "Casque",
+            "price": 150
+        }
+    ]
+};
